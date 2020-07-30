77c080adf467202ec91cfc8ea3bac64d9e72f2e3
tekel90 <you@example.com>
2020-07-30 17:12:24 -0300

    Sentencias

---
+++ JavaScript 2020 Curso desde Principiante hasta Profesional/03-Sentencias/app.js
@@ -0,0 +1,81 @@
+/**********************************************
+* Sentencia if / else
+**********************************************/
+
+/* var nombre = 'pablo';
+var estadocivil = 'soltero';
+
+if (estadocivil === 'casado'){
+    //true
+    console.log(nombre + ' esta casado');
+}else{
+    //false
+    console.log(nombre + ' esta soltero');
+} */
+
+/**********************************************
+* Sentencia Condicionales
+**********************************************/
+
+/* var nombre = 'Pablo';
+var edad = 15;
+ */
+// edad < 12 es un niño.
+// edad > 11, es < 18, es adolescente.
+// edad > 17, es < 60, es adulto.
+// edad > 60, es anciano.
+
+/* if (edad<12) {
+    console.log(nombre + ' es un niño.');
+}else if((edad>11) && (edad<18)){
+    console.log(nombre + ' es un adolescente.');
+}else if((edad>17) && (edad<60)){
+    console.log(nombre + ' es un adulto.');
+}else{
+    console.log(nombre + ' es un anciano.');
+} */
+
+
+/**********************************************
+* Operador ternario
+**********************************************/
+
+/* var nombre = 'Pablo';
+var edad = 19;
+
+edad>= 18 ? console.log(nombre + ' es mayor de edad') : console.log(nombre + ' no es mayor de edad'); */
+
+/**********************************************
+* Sentencia Switch
+**********************************************/
+
+/* var mes = 8;
+switch(mes){
+    case 1:
+        console.log('Enero');
+        break;
+    case 2:
+        console.log('Febrero');
+        break;
+    case 3:
+        console.log('Marzo');
+        break;
+    case 4:
+        console.log('Abril');
+        break;
+    case 5:
+        console.log('Mayo');
+        break;
+    default:
+        console.log('Mes no considerado');
+} */
+
+/**********************************************
+* Sentencias repetitivas - bucles
+* for
+* while
+* do..while
+**********************************************/
+
+// sentencia for
+console.log();
